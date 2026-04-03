a0d5f45458ab1e13c935be6dd9dfd9927e86d597
SentPort <photojournalist86@gmail.com>
2026-04-03 05:39:35 -0400

    Updated SearchResults.tsx

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -98,7 +98,6 @@ export default function SearchResults() {
 
   useEffect(() => {
     if (query) {
-      setWikipediaSpellSuggestion(null);
       performSearch(query);
     }
   }, [query, includeExternalContent]);
